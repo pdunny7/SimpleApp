f70b15693673b63df5d202d01f92f31b0a56104f
pjd-97 <patrick@cointracker.io>
2022-09-14 12:44:28 -0700

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Patrick Dunstone on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
